895e6e108352aa141d88f150364fb80c87b836f4
Xiomara Joya <xiomarajoya@gmail.com>
2022-10-05 23:07:46 +0200

    G_ exercise 1

--- week-2/Homework/Part-2/G-object-loops/exercise-1.js
+++ week-2/Homework/Part-2/G-object-loops/exercise-1.js
@@ -7,7 +7,11 @@ const studentGrades = {
     george: 17,
     abdul: 19,
   };
-  
+  for (const key in studentGrades) {
+    if (studentGrades[key]>18) {
+     console.log(key.toUpperCase(), studentGrades[key]);
+    }
+  }
   // Prints
   // TOM - 20
   // ABDUL - 19
